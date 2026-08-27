0f399946a96251275f14d260c0a6cf2334686498
Alexander Starovoitov <alexanderbyshnyov@gmail.com>
2026-08-27 07:13:13 +0300

    Corrected Homework 4

--- Homework 4/Script-6.sql
+++ Homework 4/Script-6.sql
@@ -1,36 +1,39 @@
 --1
-/*SELECT p.projectname
-FROM projects p
-join employees e on p.projectid = e.employeeid
-where exists (
-	select 1
-	from employeeprojects e2
-	where e2.employeeid = e.employeeid and e2.hoursworked > 150
-)*/
-
---2
-/*update projects
-set budget = budget * 1.1
+/*select p.projectname
+from projects p
+join employeeprojects ep on ep.projectid = p.projectid
 where exists (
 	select 1
 	from employees e
-	join employeeprojects e2 on e2.employeeid = e.employeeid
-	where e.department = 'Senior IT'
+	where ep.employeeid = e.employeeid and e.firstname = 'Bob' and e.lastname = 'Johnson' and ep.hoursworked > 150
+)*/
+
+--2
+/*update projects p
+set budget = budget * 1.1
+where exists (
+	select 1
+	from employeeprojects ep
+	join employees e on e.employeeid = ep.employeeid
+	where ep.projectid = p.projectid and e.department = 'Senior IT'
 )*/
 
 --3
 /*update projects
-set startdate = startdate + INTERVAL '1 year'
+set enddate = startdate + INTERVAL '1 year'
 where enddate is NULL*/
 
 --4
-begin transaction;
+/*begin transaction;
 
-insert into employees (firstname, lastname, department, salary, email)
-values ('Alexander', 'Gray', 'IT', 150000, 'gray.6907a@company.com')
-returning employeeid;
+with inserted_employee as (
+	insert into employees (firstname, lastname, department, salary, email)
+	values ('Alexander', 'Gray', 'IT', 150000, 'gray.6907a@company.com')
+	returning employeeid
+)
 
 insert into employeeprojects (employeeid, projectid, hoursworked)
-values (7, 1, 80);
+select employeeid, 1, 80
+from inserted_employee;
 
-commit;
+commit;*/
